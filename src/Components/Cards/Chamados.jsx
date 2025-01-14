87ff00f8364973d11470f56466c4050124deef19
berbraga <bernardobraga75@gmail.com>
2025-01-14 08:14:48 -0300

    berbraga: deploy

--- src/Components/Cards/Chamados.jsx
+++ src/Components/Cards/Chamados.jsx
@@ -13,6 +13,7 @@ function Chamados({ chat, currentChat, onSelect }) {
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
+    //date.setMinutes(date.getMinutes() + 10); // nao funcionou
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
